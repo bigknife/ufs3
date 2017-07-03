751a1625cf7fb076cb20ecb52f32ce6a9aee5b57
大刀 <zenhue.song@gmail.com>
2017-07-03 18:54:55 +0800

    add core api file

---
+++ core/src/main/scala/api.scala
@@ -0,0 +1,26 @@
+/**
+  * api.scala
+  * ---------
+  * small programs composed with kernel's adt
+  * @author: bigknife@outlook.com
+  * @create: 2017/07/03
+  */
+package ufs3
+package core
+
+import cats.free.{Free}
+import Free._
+
+import ufs3.kernel.log._
+import ufs3.kernel.store._
+import ufs3.kernel.audit._
+import ufs3.kernel.backup._
+import ufs3.kernel.stream._
+
+import cats.effect.IO
+import java.io.InputStream
+
+
+object api {
+
+}
